a8d4006d1939e0462c039ffa397da51103ac8920
malathi <malathi@Deepak>
2024-10-08 09:58:28 +0530

    ArmStrong

--- Java_Problems/src/main/java/armstrong_Numbers/Checking_Armstrong_Number_2.java
+++ Java_Problems/src/main/java/armstrong_Numbers/Checking_Armstrong_Number_2.java
@@ -5,12 +5,7 @@ import java.util.Scanner;
 public class Checking_Armstrong_Number_2 {
 
 	public static void main(String[] args) {
-		//Armstrong number is a number that is equal to the sum of cubes of its digits.
-		// 153 is an Armstrong number. ==> (1*1*1) + (5*5*5) + (3*3*3) = 153
-
-		// Scanner sc = new Scanner(System.in);
-	      //  System.out.print("Enter a number to check if it is an Armstrong number: ");
-	     //   int num = sc.nextInt(); // User input for the number to check
+		
 		
 		int num = 153;// It is the number to check Armstrong
 	        int sum = 0, lastDigit, temp = num; // Temporary variable to hold the original number
@@ -32,3 +27,10 @@ public class Checking_Armstrong_Number_2 {
 	}
 
 }
+
+//Armstrong number is a number that is equal to the sum of cubes of its digits.
+		// 153 is an Armstrong number. ==> (1*1*1) + (5*5*5) + (3*3*3) = 153
+
+		// Scanner sc = new Scanner(System.in);
+	      //  System.out.print("Enter a number to check if it is an Armstrong number: ");
+	     //   int num = sc.nextInt(); // User input for the number to check
